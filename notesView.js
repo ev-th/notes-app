936817e445f5fb28740ee28d4d47569ce48d0aac
Evan Thomas <evanthomas0292@gmail.com>
2023-05-31 12:02:09 +0100

    Update view to use a text box and button

--- notesView.js
+++ notesView.js
@@ -2,15 +2,33 @@ class NotesView {
   constructor(model) {
     this.model = model
     this.mainContainerEl = document.querySelector('#main-container');
+    this.textBox = document.querySelector('#text-input');
+    this.button = document.querySelector('#add-note-button');
+
+    this.button.addEventListener('click', () => {
+      const newNote = this.textBox.value;
+      this.addNote(newNote);
+    })
+  }
+
+  addNote(note) {
+    this.model.addNote(note)
+    this.textBox.value = ''
+    this.displayNotes();
   }
 
   displayNotes() {
+    this.hideNotes();
     this.model.getNotes().forEach((note) => {
       let div = document.createElement('div')
       div.textContent = note
       div.classList.add('note')
       this.mainContainerEl.append(div)
     })
+  }
+
+  hideNotes() {
+    this.mainContainerEl.querySelectorAll('.note').forEach(note => note.remove());
   }
 }
 
--- notesView.test.js
+++ notesView.test.js
@@ -20,4 +20,36 @@ describe ('NotesView', () => {
     expect(document.querySelectorAll('.note')[0].textContent).toEqual('Hello World')
     expect(document.querySelectorAll('.note').length).toEqual(1)
   })
+
+  it('displays a new note on the page', () => {
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    view.textBox.value = 'Buy milk';
+    view.button.click();
+    expect(document.querySelectorAll('.note').length).toEqual(1);
+    expect(document.querySelectorAll('.note')[0].textContent).toEqual('Buy milk');
+  })
+
+  it('displays two notes on the page', () => {
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    view.textBox.value = 'Buy milk';
+    view.button.click();
+    view.textBox.value = 'Do laundry';
+    view.button.click();
+    expect(document.querySelectorAll('.note').length).toEqual(2);
+    expect(document.querySelectorAll('.note')[0].textContent).toEqual('Buy milk');
+    expect(document.querySelectorAll('.note')[1].textContent).toEqual('Do laundry');
+  })
+
+  it('clears the value of the text box when new note is created', () => {
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    view.textBox.value = 'Buy milk';
+    view.button.click();
+    expect(view.textBox.value).toEqual('')
+  })
 })
